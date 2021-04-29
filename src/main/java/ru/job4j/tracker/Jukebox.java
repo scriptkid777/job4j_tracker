566a70665dc167531e53e1073f5653f506c5e7a9
$(scriptkid777) <$(scriptkid777)@users.noreply.github.com>
2021-04-29 14:40:00 +0700

    1.3. Вызов метода с аргументами. [#485765]

---
+++ src/main/java/ru/job4j/tracker/Jukebox.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+public class Jukebox {
+    public void music(int position){
+       if (position == 1){
+           System.out.println("Пусть бегут неуклюже");
+       }
+       else if (position == 2) {
+           System.out.println("Спокойной ночи");
+       }
+       else{
+           System.out.println("Песня не найдена");
+       }
+       }
+
+    public static void main(String[] args) {
+        Jukebox play = new Jukebox();
+        play.music(1);
+        play.music(2);
+        play.music(3);
+    }
+}
