76c3a52b75ebc1d887b14e8baed2bd5fc03efbb4
$(scriptkid777) <$(scriptkid777)@users.noreply.github.com>
2021-05-06 09:53:06 +0700

    1.7. Конструктор [#485769]

---
+++ src/main/java/ru/job4j/tracker/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.tracker;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void print() {
+        System.out.println("Active:" + active);
+        System.out.println("Status:" + status);
+        System.out.println("Message:" + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error1 = new Error(true,1,"Ошибка");
+        error.print();
+        error1.print();
+    }
+}
