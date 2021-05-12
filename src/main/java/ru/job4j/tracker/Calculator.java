cd97cc21de170aba0bcb3cdf0398ee90b4c96349
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-05-12 13:36:18 +0700

    2.1. Рефакторинг - Расстояние между точками. [#485758]

---
+++ src/main/java/ru/job4j/tracker/Calculator.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+public class Calculator {
+    public static double add(double first, double second) {
+        return first + second;
+    }
+
+    public static double add(double first, double second, double third) {
+        return add(
+                first,
+                add(second, third)
+        );
+    }
+
+    public static double add(double first, double second, double third, double fourth){
+        return add(first, second, add(third,fourth));
+    }
+
+}
---
+++ src/test/java/ru/job4j/tracker/CalculatorTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void when4number(){
+        double expected = 11;
+        double out = Calculator.add(1,2,3,5);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
