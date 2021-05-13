2a0cdca27e9f152c537d6c20053045f1023e2276
$(scriptkid777) <$(scriptkid777)@users.noreply.github.com>
2021-05-13 15:19:19 +0700

    3. Перегрузить метод max для трех чисел [#122644 #46564]

--- src/main/java/ru/job4j/tracker/Calculator.java
+++ src/main/java/ru/job4j/tracker/Calculator.java
@@ -2,20 +2,15 @@ package ru.job4j.tracker;
 
 public class Calculator {
     public  double max(double first, double second) {
-        if (first > second) {
-            return first;
-        }
-        else {
-            return second;
-        }
+       return  (first > second) ?  first : second;
     }
 
     public  double max(double first, double second, double third) {
         return max(first, max(second, third));
     }
 
-    public  double max(double first, double second, double third, double fourth){
-        return max(first, max(second, max(third,fourth)));
+    public  double max(double first, double second, double third, double fourth) {
+        return max(first, max(second, max(third, fourth)));
     }
 
 }
