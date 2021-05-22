148de0049284ddeff766676aec2e1c07ac2e75a7
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-05-22 18:09:22 +0700

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/tracker/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Alexey Ivanovich Ivanov");
+        student.setGroup("pi-04");
+        student.setReceipt(new Date(7760000));
+        System.out.println(student.getFio()+"  "+student.getGroup()+"  "+ student.getReceipt());
+    }
+}
