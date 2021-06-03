ea14c68238e7490b6d96957cf16c66172233d6e7
$(scriptkid777) <$(scriptkid777)@users.noreply.github.com>
2021-06-03 17:46:53 +0700

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -50,7 +50,7 @@ public class Tracker {
 
     private int indexOf(int id) {
         int rsl = -1;
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (items[i].getId() == id) {
                 rsl = i;
                 break;
