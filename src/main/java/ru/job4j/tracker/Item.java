518947bdd2d34ff6eed5300380982f2ae5b56946
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-06-06 00:28:30 +0700

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -10,6 +10,15 @@ public class Item {
     public Item() {
     }
 
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created +
+                '}';
+    }
+
     public Item(String name) {
         this.name = name;
     }
---
+++ src/main/java/ru/job4j/tracker/oop/StartU.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker.oop;
+
+import ru.job4j.tracker.Item;
+
+public class StartU {
+
+    public static void main(String[] args) {
+        Item created = new Item();
+        System.out.println(created);
+    }
+
+}
