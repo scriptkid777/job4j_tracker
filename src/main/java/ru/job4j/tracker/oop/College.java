308cab22f64131a253a0f180a785d4df36df7b36
$(scriptkid777) <$(scriptkid777)@users.noreply.github.com>
2021-06-24 10:57:35 +0700

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/tracker/oop/College.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman(); // Объект класса Freshman
+        Student student = freshman; //приведение к типу родителя Student, up casting
+        Object obj = freshman; //приведение к типу родителя Object, up casting
+    }
+}
