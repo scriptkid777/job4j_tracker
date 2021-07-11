4d12465787df2f38ae86ed9046f8d281473fa36d
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-07-11 20:15:55 +0700

    9.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/ExitAction.java
+++ src/main/java/ru/job4j/tracker/ExitAction.java
@@ -3,7 +3,7 @@ package ru.job4j.tracker;
 public class ExitAction implements UserAction {
     @Override
     public String name() {
-        return "=== Exiting the program ===";
+        return "Exit";
     }
 
     @Override
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
+    }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
@@ -21,10 +26,11 @@ public class StartUI {
         }
 
         public static void main(String[]args) {
+        Output output = new ConsoleOutput();
             Input input = new ConsoleInput();
             Tracker tracker = new Tracker();
             UserAction[] actions = {
-                    new CreateAction(),
+                    new CreateAction(output),
                     new DeleteAction(),
                     new EditAction(),
                     new FindIdAction(),
@@ -32,7 +38,9 @@ public class StartUI {
                     new FindAllAction(),
                     new ExitAction()
             };
-            new StartUI().init(input, tracker, actions);
+            new StartUI(output).init(input, tracker, actions);
         }
+
+
     }
 
