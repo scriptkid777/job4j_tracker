24e2ab1450b070068457172e84c3654357d4580d
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-07-19 02:20:07 +0700

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #179053]

--- src/main/java/ru/job4j/tracker/exceptions/Fact.java
+++ src/main/java/ru/job4j/tracker/exceptions/Fact.java
@@ -7,13 +7,12 @@ public class Fact {
 
     public static int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("n должно быть больше 0!");
+            throw new IllegalArgumentException("n должно быть равно  либо больше 0!");
         }
         int rsl = 1;
         for (int index = 1; index <= n; index++) {
             rsl *= index;
         }
-        System.out.println(rsl);
         return rsl;
     }
 }
