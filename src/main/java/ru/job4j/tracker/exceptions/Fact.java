c0b016b8e5bfbdca39b82e75bdc11b7b87928c6d
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-07-19 00:50:33 +0700

    0.3. Тестирование исключений с junit [#219366]

--- src/main/java/ru/job4j/tracker/exceptions/Fact.java
+++ src/main/java/ru/job4j/tracker/exceptions/Fact.java
@@ -2,10 +2,10 @@ package ru.job4j.tracker.exceptions;
 
 public class Fact {
     public static void main(String[] args) {
-        new Fact().calc(3);
+        calc(-1);
     }
 
-    public int calc(int n) {
+    public static int calc(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("n должно быть больше 0!");
         }
---
+++ src/test/java/ru/job4j/tracker/exceptions/FactTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker.exceptions;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNlessZero() {
+       int rsl = Fact.calc(-2);
+        assertThat(rsl,is(2));
+    }
+}
