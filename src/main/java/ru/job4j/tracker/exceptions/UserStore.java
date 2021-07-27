2b22f82a1dc6ae82caa99c5d073a97842253f9db
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-07-28 00:16:47 +0700

    0.5. Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/tracker/exceptions/UserStore.java
+++ src/main/java/ru/job4j/tracker/exceptions/UserStore.java
@@ -19,7 +19,7 @@ public class UserStore {
 
     public static void main(String[] args) {
         User[] users = {
-                new User("Petr Arsentev", false)
+                new User("Petr Arsentev", true)
         };
         User user = null;
         try {
