e213b3dd612d234ed36fc59a935eeaefa52822e4
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-07-29 16:23:29 +0700

    0.5. Иерархия исключений и множественный catch. [#219368 #182101]

--- src/main/java/ru/job4j/tracker/exceptions/UserStore.java
+++ src/main/java/ru/job4j/tracker/exceptions/UserStore.java
@@ -2,9 +2,9 @@ package ru.job4j.tracker.exceptions;
 
 public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
-        for (int i = 0; i < users.length; i++) {
-            if (login.equals(users[i].getUsername())) {
-              return users[i];
+        for (User user : users) {
+            if (login.equals(user.getUsername())) {
+                return user;
             }
         }
     throw new UserNotFoundException("Юзер не найден ");
