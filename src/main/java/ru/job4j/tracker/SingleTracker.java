8094df586ba8a38b9a44312b4025885dfdb90729
$(scriptkid777) <$(scriptkid777)@users.noreply.github.com>
2021-08-13 11:36:22 +0700

    2. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -27,4 +27,15 @@ public final class SingleTracker {
         return tracker.findById(id);
     }
 
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
 }
