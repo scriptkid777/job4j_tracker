95869664919e9e9484f178a8b1bd2d28167ada61
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-10-03 21:16:43 +0700

    3. Модели данных и HashSet. [#218530 #211435]

--- src/test/java/ru/job4j/tracker/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/tracker/collection/NotifyAccountTest.java
@@ -25,4 +25,20 @@ public class NotifyAccountTest {
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
 
+    @Test
+    public void deleteDuplicat() {
+        List<Account> accounts = List.of(
+                new Account("123", "Petr Arsentev", "binbvvc?"),
+                new Account("123", "Petr Arsentev", "binbvvc?"),
+                new Account("125", "Petr Arsentev", "q0w9er")
+        );
+        HashSet<Account> expected = new HashSet<>(
+                List.of(
+                        new Account("123", "Petr Arsentev", "binbvvc?"),
+                        new Account("125", "Petr Arsentev", "q0w9er")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expected));
+    }
+
 }
