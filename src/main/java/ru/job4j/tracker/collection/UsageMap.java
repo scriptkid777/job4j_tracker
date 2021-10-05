2cfe67afd8e9616346c71272accdf0f1b23231a6
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-10-05 22:09:49 +0700

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/tracker/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker.collection;
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("fourme@bk.ru", "Vadim Nechalov");
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
