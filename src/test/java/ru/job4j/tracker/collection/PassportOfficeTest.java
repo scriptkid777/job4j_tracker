2db77ac79b3340a299d7e257549ad7a8ea8fb566
$(scriptkid777) <$(scriptkid777)@users.noreply.github.com>
2021-10-12 11:08:12 +0700

    1. Паспорт и жители [#212656 #214436]

--- src/test/java/ru/job4j/tracker/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/tracker/collection/PassportOfficeTest.java
@@ -7,12 +7,21 @@ import static org.junit.Assert.*;
 
 public class PassportOfficeTest {
     @Test
-
     public void add() {
         Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void check() {
+        Citizen citizen = new Citizen("2e55f", "Petr");
+        Citizen citizen1 = new Citizen("2e55f","Alex");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen1);
+        assertFalse(office.get(citizen.getPassport()) != office.get(citizen1.getPassport()));
     }
 
 }
