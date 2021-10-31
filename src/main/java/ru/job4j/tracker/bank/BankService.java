5af78661c497c93042214c410cbf6f122d1a3f8c
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2021-10-31 22:59:12 +0700

    3. Банковские переводы [#10038]

--- src/main/java/ru/job4j/tracker/bank/BankService.java
+++ src/main/java/ru/job4j/tracker/bank/BankService.java
@@ -47,12 +47,13 @@ public class BankService {
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-      Account srcAccount = findByRequisite(srcPassport, srcRequisite);
-      if (srcAccount != null) {
-          Account destAccount = findByRequisite(destPassport, destRequisite);
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && destAccount != null && srcAccount.getBalance() >= amount) {
+          srcAccount.setBalance((srcAccount.getBalance() - amount));
           destAccount.setBalance(destAccount.getBalance() + amount);
           rsl = true;
-      }
+        }
         return rsl;
     }
 }
