2b3a24678ed6f4843c888ef64a6a1ea888f719cc
$(scriptkid777) <$(scriptkid777)@users.noreply.github.com>
2021-12-28 16:46:56 +0700

    1.1. Анонимные классы [#214142 #244405]

--- src/main/java/ru/job4j/tracker/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/tracker/lambda/AttachmentSort.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+
 public class AttachmentSort {
     public static void main(String[] args) {
         List<Attachment> attachments = Arrays.asList(
