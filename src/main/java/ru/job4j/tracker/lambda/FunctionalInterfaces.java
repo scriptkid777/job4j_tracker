3d0431709c3db9b54dcce5d17951a20219892c00
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2022-01-05 00:29:19 +0700

    1.2. Функциональный интерфейс [#247497]

---
+++ src/main/java/ru/job4j/tracker/lambda/FunctionalInterfaces.java
@@ -0,0 +1,39 @@
+package ru.job4j.tracker.lambda;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.*;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        List<String> list = List.of("one", "two", "three",
+                "four", "five", "six", "seven");
+
+        BiConsumer<Integer, String> biCon = (i, s1) -> map.put(i + 1, list.get(i));
+
+        for (int i = 0; i < list.size(); i++) {
+            biCon.accept(i, list.get(i));
+        }
+        System.out.println(map.toString());
+
+        BiPredicate<Integer, String> biPred = (i, s) -> (i % 2 == 0 || map.get(i).length() == 4);
+        for (Integer i : map.keySet()) {
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
+            }
+        }
+
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+
+        Consumer<String> con = (s) -> System.out.println(s);
+
+        Function<String, String> func = (fs) -> fs.toUpperCase();
+
+        for (String str  : sup.get()) {
+            con.accept(func.apply(str));
+        }
+    }
+}
