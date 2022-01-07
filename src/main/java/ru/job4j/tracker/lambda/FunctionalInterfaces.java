0007a20638830c3af79e75238a2d2d279c66a8b7
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2022-01-08 00:42:50 +0700

    1.2. Функциональный интерфейс [#247497 #247018]

--- src/main/java/ru/job4j/tracker/lambda/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/tracker/lambda/FunctionalInterfaces.java
@@ -14,7 +14,7 @@ public class FunctionalInterfaces {
         BiConsumer<Integer, String> biCon = (i, s) -> map.put(i, s);
 
         for (int i = 0; i < list.size(); i++) {
-            biCon.accept(i, list.get(i));
+            biCon.accept(i + 1, list.get(i));
         }
         System.out.println(map.toString());
 
