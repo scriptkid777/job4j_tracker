01c3750523117b06ea34980e4c969487ef21dd40
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2022-01-08 23:46:54 +0700

    1.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/tracker/lambda/SearchFolder.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFolder {
+
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
+        List<Folder> rsl = new ArrayList<>();
+        for (Folder folder : list) {
+            if (pred.test(folder)) {
+                rsl.add(folder);
+            }
+        }
+        return rsl;
+    }
+}
