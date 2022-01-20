6b57190eb1ad90b26299dcde92f3a84f62eb9348
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2022-01-20 18:38:42 +0700

    4. Зона видимости в лямбда-выражениях и исключения [#70609]

--- src/main/java/ru/job4j/tracker/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/tracker/lambda/ScopeInside.java
@@ -6,11 +6,10 @@ public class ScopeInside {
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
         int total = 0;
-        for (int i = 0; i < number.length; i++) {
-            int num = i;
+        for (int numb:number) {
             int finalTotal = total;
             total = add(
-                    () -> finalTotal + num
+                    () -> finalTotal + numb
             );
         }
         System.out.println(total);
