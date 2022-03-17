606f4df824fa39f4616f833c0de6f69853cad532
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2022-03-17 15:36:46 +0700

    8. Перенос методов в Stream API [#279131]

--- src/main/java/ru/job4j/tracker/stream/Card.java
+++ src/main/java/ru/job4j/tracker/stream/Card.java
@@ -23,7 +23,7 @@ public class Card {
     public static void main(String[] args) {
         Stream.of(Suit.values())
                 .flatMap(suit1 -> Stream.of(Value.values())
-                .map(value1 -> suit1 + " " + value1))
+                .map(value1 -> new Card(suit1,value1)))
                 .forEach(System.out::println);
     }
 }
