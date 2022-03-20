4e3c87b92a03c893b7573ee6d53e49cff940cef8
scriptkid777 <44677406+scriptkid777@users.noreply.github.com>
2022-03-20 23:44:31 +0700

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/tracker/collection/FullSearchTest.java
+++ src/test/java/ru/job4j/tracker/collection/FullSearchTest.java
@@ -2,7 +2,6 @@ package ru.job4j.tracker.collection;
 
 import org.junit.Test;
 import java.util.HashSet;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -13,13 +12,13 @@ public class FullSearchTest {
 
     @Test
     public void extractNumber() {
-        List<Task> tasks = Arrays.asList(
+        List<Task> tasks = List.of(
                 new Task("1", "First desc"),
                 new Task("2", "Second desc"),
                 new Task("1", "First desc")
         );
 
-        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
+        Set<String> expected = new HashSet<>(List.of("1", "2"));
         assertThat(FullSearch.extractNumber(tasks), is(expected));
     }
 
--- src/test/java/ru/job4j/tracker/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/tracker/collection/NotifyAccountTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker.collection;
 
 import org.junit.Test;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -12,12 +11,12 @@ public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
