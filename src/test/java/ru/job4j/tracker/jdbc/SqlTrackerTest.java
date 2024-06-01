3945b5afd113b5e87b35235ae6313d0a19ededfb
scriptkid777 <servsystem@yandex.ru>
2024-06-01 23:58:35 +0700

    3. Liquibase. Интеграционные тесты. [#504894 #497825]

--- src/test/java/ru/job4j/tracker/jdbc/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/jdbc/SqlTrackerTest.java
@@ -14,7 +14,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SqlTrackerTest {
     private static Connection connection;
