2dd1dfb87d1382bb6636777568aee65dc4fc725c
scriptkid777 <servsystem@yandex.ru>
2025-10-21 23:22:00 +0700

    1. Lombok. Настройка IDEA [#504888]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,8 +2,9 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Item implements Comparable<Item> {
 
     private static final DateTimeFormatter FORMATTER =
@@ -13,9 +14,6 @@ public class Item implements Comparable<Item> {
     private String name;
 
     private LocalDateTime created = LocalDateTime.now().withNano(0);
-
-    public Item() {
-    }
 
     public Item(String name) {
         this.name = name;
@@ -27,62 +25,9 @@ public class Item implements Comparable<Item> {
         this.created = created;
     }
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
-        this.created = created;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("id: %s, name: %s, created: %s", id, name, FORMATTER.format(created));
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public int compareTo(Item o) {
         return Integer.compare(id, o.id);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Item item = (Item) o;
-
-        if (id != item.id) {
-            return false;
-        }
-        return Objects.equals(name, item.name);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
     }
 }
 
